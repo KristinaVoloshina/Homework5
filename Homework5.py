dab1069748bb48b14599306935b149d895c0dfa3
Kristina Voloshina <christina_k@inbox.ru>
2022-10-11 11:20:27 +0500

    All tasks

--- Homework5.py
+++ Homework5.py
@@ -18,7 +18,8 @@ def bot_calc(value):
 player1 = input("Введите имя первого игрока: ")
 player2 = "Bot"
 value = int(input("Введите количество конфет на столе: "))
-flag = random.randint(0,2) # флаг очередности
+from random import randint
+flag = randint(0,2) # флаг очередности
 if flag:
     print(f"Первый ходит {player1}")
 else:
@@ -45,3 +46,98 @@ if flag:
     print(f"Выиграл {player1}")
 else:
     print(f"Выиграл {player2}")
+
+    
+
+    # Крестики - нолики
+
+    print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
+
+board = list(range(1,10))
+
+def draw_board(board):
+   print("-" * 13)
+   for i in range(3):
+      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+      print("-" * 13)
+
+def take_input(player_token):
+   valid = False
+   while not valid:
+      player_answer = input("Куда поставим " + player_token+"? ")
+      try:
+         player_answer = int(player_answer)
+      except:
+         print("Некорректный ввод. Вы уверены, что ввели число?")
+         continue
+      if player_answer >= 1 and player_answer <= 9:
+         if(str(board[player_answer-1]) not in "XO"):
+            board[player_answer-1] = player_token
+            valid = True
+         else:
+            print("Эта клетка уже занята!")
+      else:
+        print("Некорректный ввод. Введите число от 1 до 9.")
+
+def check_win(board):
+   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+   for each in win_coord:
+       if board[each[0]] == board[each[1]] == board[each[2]]:
+          return board[each[0]]
+   return False
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+           take_input("X")
+        else:
+           take_input("O")
+        counter += 1
+        if counter > 4:
+           tmp = check_win(board)
+           if tmp:
+              print(tmp, "выиграл!")
+              win = True
+              break
+        if counter == 9:
+            print("Ничья!")
+            break
+    draw_board(board)
+main(board)
+
+input("Нажмите Enter для выхода!")
+
+
+# Сжатие
+
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+
+s = input("Введите текст для кодировки: ")
+print(f"Текст после кодировки: {coding(s)}")
+print(f"Текст после дешифровки: {decoding(coding(s))}")
